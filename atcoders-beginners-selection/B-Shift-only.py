2cb6d78cc6550714e2d2a1b1301159097b3342c3
S.Shohei <shoukk8@gmail.com>
2026-05-05 11:38:53 +0800

    Add function to count halving operations on list
    Implement a function to count how many times all elements can be halved.

--- atcoders-beginners-selection/B-Shift-only.py
+++ atcoders-beginners-selection/B-Shift-only.py
@@ -1,1 +1,17 @@
+def solve():
+    # 入力
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    count = 0
+    
+    while True:
+        if any(x % 2 != 0 for x in a):
+            break
+        a = [x // 2 for x in a]
+        count += 1
+    
+    print(count)
 
+if __name__ == "__main__":
+    solve()
