95fda41212efc8eecd7abf23e3dd526726746d0f
S.Shohei <shoukk8@gmail.com>
2026-05-05 11:40:38 +0800

    Update B-Coins.py

--- atcoders-beginners-selection/B-Coins.py
+++ atcoders-beginners-selection/B-Coins.py
@@ -1,1 +1,11 @@
+from itertools import product
 
+A = int(input())
+B = int(input())
+C = int(input())
+X = int(input())
+count = 0
+for a, b, c in product(range(A + 1), range(B + 1), range(C + 1)):
+    if 500 * a + 100 * b + 50 * c == X:
+        count += 1
+print(count)
