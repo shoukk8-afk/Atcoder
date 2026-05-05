07c3a7410a91eded0880481186476b525402f0ee
S.Shohei <shoukk8@gmail.com>
2026-05-05 11:42:01 +0800

    Update B-Some-Sums.py

--- atcoders-beginners-selection/B-Some-Sums.py
+++ atcoders-beginners-selection/B-Some-Sums.py
@@ -1,1 +1,12 @@
-
+N, A, B = map(int, input().split())
+total = 0
+for n in range(1, N + 1):
+    a = n
+    sum = 0
+    l = map(int, ','.join(str(n)).split(','))
+    for i in l:
+        sum += i
+    if A <= sum <= B:
+        total += a
+        
+print(total)
