cc5779ed00c49dd806b54b22318c39df1aee8480
S.Shohei <shoukk8@gmail.com>
2026-05-05 11:35:45 +0800

    Update A-Product.py

--- atcoders-beginners-selection/A-Product.py
+++ atcoders-beginners-selection/A-Product.py
@@ -1,5 +1,6 @@
-a,b = map(int,input().split(' '))
-if (a*b)%2 == 0:
-  print('Even')
+a, b = map(int, input().split())
+c = a * b
+if c % 2 == 0:
+  print("Even")
 else:
-  print('Odd')
+  print("Odd")
